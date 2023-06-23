121341a20acf0228ce9fb7275bc907bd9e833c8d
Rita Kozlov <rita@cloudflare.com>
2023-06-23 15:44:28 -0400

    upadte og

--- src/config.ts
+++ src/config.ts
@@ -9,4 +9,4 @@ export const MY_NAME = "rita kozlov";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
-export const SITE_URL = BASE_URL.origin;
+export const SITE_URL = "https://ritakozlov.com";
